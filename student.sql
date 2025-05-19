87e4808aa75a2cec875c6ea200603baee21b9d78
ameso213 <142228988+ameso213@users.noreply.github.com>
2025-05-19 23:10:58 +0300

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,52 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 18, 2025 at 04:26 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `exam`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `student`
---
-
-CREATE TABLE `student` (
-  `Reg_No` varchar(10) DEFAULT NULL,
-  `First_Name` varchar(50) DEFAULT NULL,
-  `Last_Name` varchar(50) DEFAULT NULL,
-  `Course` varchar(10) DEFAULT NULL,
-  `Gender` varchar(10) DEFAULT NULL,
-  `D_O_B` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `student`
---
-
-INSERT INTO `student` (`Reg_No`, `First_Name`, `Last_Name`, `Course`, `Gender`, `D_O_B`) VALUES
-('2016', 'Susan', 'Mwangi', 'DIT', 'Female', '1978-11-13'),
-('2016', 'Steve', 'John', 'DCS', 'Male', '1989-11-13'),
-('2017', 'Kirigo', 'Mutua', 'DIT', 'Female', '1990-11-13'),
-('2016', 'Kipkorir', 'Mulu', 'DCS', 'Male', '1989-11-13');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  
